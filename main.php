fc7994e7e3b1b2de260488b7197ef0745e41f549
bizzl <bizzl@users.sourceforge.net>
2011-04-05 11:59:38 +0200

    Initial commit.

---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wordpress Video Plugin Helper
+Plugin URI: 
+Description: Adds a tab to the upload dialog to make the use of the video plugin easier.
+Version: 1.0.0
+Author: Benjamin Kleiner <bizzl@users.sourceforge.net>
+Author URI: 
+License: LGPL3
+*/
+?>
+ 
