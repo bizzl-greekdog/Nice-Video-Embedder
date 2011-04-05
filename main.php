7e8febc1a7122a308a5637c8557a64f05d0c1039
bizzl <bizzl@users.sourceforge.net>
2011-04-05 15:35:15 +0200

    IT'S ALIVE!

--- main.php
+++ main.php
@@ -9,20 +9,31 @@ Author URI:
 License: LGPL3
 */
 
+$fromvideoplatform_rules = array(
+	'#youtube\.com/watch\?.*v=(?P<ID>.{11})#i' => '[youtube %ID%]',
+	'#vimeo\.com/(clip:)?(?P<ID>[0-9]+)#i' => '[vimeo %ID% %width% %height%]',
+);
+
+function nsprintf($subject, $arguments) {
+	foreach ($arguments as $key => $value)
+		$subject = str_replace ("%$key%", $value, $subject);
+	return $subject;
+}
+
 function fromvideoplatform_media_menu($tabs) {
+	if ($_REQUEST['type'] != 'video')
+		return $tabs;
 	$newtab = array('fromvideoplatform' => __('From Video Platform', 'fromvideoplatform'));
 	return array_merge($tabs, $newtab);
 }
-add_filter('media_upload_tabs', 'fromvideoplatform_media_menu');
 
-function media_fromvideoplatform_form() {
+function media_fromvideoplatgorm_process($preset) {
  	media_upload_header();
 	$post_id = intval($_REQUEST['post_id']);
 
-	$form_action_url = admin_url("media-upload.php?tab=fromvideoplatform&post_id=$post_id");
+	$form_action_url = admin_url("media-upload.php?type=video&tab=fromvideoplatform&post_id=$post_id");
 	$form_action_url = apply_filters('media_upload_form_url', $form_action_url, $type);
 
-	$callback = "media_fromvideoplatgorm_process";
 	printf('<form enctype="multipart/form-data" method="post" action="%s" class="media-upload-form type-form validate" id="fromvideoplatform-form">', esc_attr($form_action_url));
 	printf('<input type="hidden" name="post_id" id="post_id" value="%s" />', $post_id);
 	wp_nonce_field('media-form');
@@ -36,7 +47,7 @@ function media_fromvideoplatform_form() {
 						<span class="alignleft"><label for="insertonly[href]">' . __('Video URL') . '</label></span>
 						<span class="alignright"><abbr title="required" class="required">*</abbr></span>
 					</th>
-					<td class="field"><input id="insertonly[href]" name="insertonly[href]" value="" type="text" aria-required="true"></td>
+					<td class="field"><input id="insertonly[href]" name="insertonly[href]" value="' . $preset . '" type="text" aria-required="true"></td>
 				</tr>
 				<tr>
 					<th valign="top" scope="row" class="label">
@@ -54,8 +65,36 @@ function media_fromvideoplatform_form() {
 }
 
 function fromvideoplatform_menu_handle() {
-	return wp_iframe('media_fromvideoplatform_form');
+	if ( !empty($_POST['insertonlybutton']) && !empty($_POST['insertonly']['href']) ) {
+		$shortcode = '';
+		$matches = array();
+		$dimensions = array('width' => 400, 'height' => 300);
+		$title = $_POST['insertonly']['title'];
+		$href = $_POST['insertonly']['href'];
+		global $fromvideoplatform_rules;
+
+		foreach ($fromvideoplatform_rules as $key => $value) {
+			if (preg_match($key, $href, &$matches) > 0) {
+				$shortcode = nsprintf($value, array_merge($matches, $dimensions));
+				break;
+			}
+		}
+
+		if (!$shortcode)
+			return wp_iframe('media_fromvideoplatgorm_process', $href);
+
+		if ($title) {
+			$arguments = array_merge(array('title' => $title, 'content' => $shortcode), $dimensions);
+			$shortcode = nsprintf('[caption id="%title%" width="%width%" caption="%title%"]%content%[/caption]', $arguments);
+		}
+
+		return media_send_to_editor($shortcode);
+	} else {
+		return wp_iframe('media_fromvideoplatgorm_process', '');
+	}
 }
+
 add_action('media_upload_fromvideoplatform', 'fromvideoplatform_menu_handle');
+add_filter('media_upload_tabs', 'fromvideoplatform_media_menu');
 ?>
  
