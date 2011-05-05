1984a7252bed12643119fa6343d80bd387cc67c8
Benjamin Kleiner <bizzl@users.sourceforge.net>
2011-05-05 14:04:48 +0200

    Rebranding

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Wordpress Video Plugin Helper
+Plugin Name: Nice Video Embedder
 Plugin URI: 
-Description: Adds a tab to the upload dialog to make the use of the video plugin easier.
-Version: 2.0.0
+Description: Adds a tab to the upload dialog and some shortcodes to make the embedding of videos easier.
+Version: 2.5.0
 Author: Benjamin Kleiner <bizzl@users.sourceforge.net>
 Author URI: 
 License: LGPL3
@@ -25,12 +25,12 @@ if (!function_exists('nsprintf')) {
 
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'tag.php');
 
-class Wordpress_Video_Plugin_Helper {
+class Nice_Video_Embedder {
 
-	protected static $domain = 'wordpress-video-plugin-helper';
+	protected static $domain = 'nice-video-embedder';
 	protected static $base = '';
 	protected static $rules = array(
-		'#youtube\.com/watch\?.*v=(?P<ID>.{11})#i' => '[youtube %ID%]',
+		'#youtube\.com/watch\?.*v=(?P<ID>.{11})#i' => '[youtube %ID% %width% %height%]',
 		'#vimeo\.com/(clip:)?(?P<ID>[0-9]+)#i' => '[vimeo %ID% %width% %height%]',
 	);
 
@@ -48,6 +48,8 @@ class Wordpress_Video_Plugin_Helper {
 		self::init_l10n();
 		add_action('media_upload_fromvideoplatform', array(__CLASS__, 'menu_handle'));
 		add_filter('media_upload_tabs', array(__CLASS__, 'media_menu'));
+		add_shortcode('youtube', array(__CLASS__,  'shortcode_handler'));
+		add_shortcode('vimeo', array(__CLASS__,  'shortcode_handler'));
 	}
 
 	public static function media_menu($tabs) {
@@ -166,9 +168,36 @@ class Wordpress_Video_Plugin_Helper {
 		}
 	}
 
+	public static function shortcode_handler($atts, $content, $tag) {
+		$result = '';
+		$id = (isset($atts[1])) ? $atts[0] : '';
+		$width = (isset($atts[2])) ? $atts[1] : 400;
+		$height = (isset($atts[3])) ? $atts[2] : 300;
+		if ($tag == 'youtube') {
+			$result = tag('object')->attr('style', "height: {$height}px; width: {$height}px")->append(
+					tag('param')->attr('name', 'movie')->attr('value', "http://www.youtube.com/v/{$id}?version=3"),
+					tag('param')->attr('name', 'allowFullScreen')->attr('value', 'true'),
+					tag('param')->attr('name', 'allowScriptAccess')->attr('value', 'always'),
+					tag('embed')->attr(array(
+						'src' => "http://www.youtube.com/v/{$id}?version=3",
+						'type' => 'application/x-shockwave-flash',
+						'allowfullscreen' => 'true',
+						'allowScriptAccess' => 'always',
+						'width' => $width,
+						'height' => $height
+					))
+			);
+		} elseif ($tag == 'vimeo') {
+			$result = tag('iframe')->attr(array(
+				'src' => 'http://player.vimeo.com/video/' . $id,
+				'width' => $width,
+				'height' => $height,
+			))->append(' ');
+		}
+		return strval($result);
+	}
 }
 
-Wordpress_Video_Plugin_Helper::init();
+Nice_Video_Embedder::init();
 
 ?>
- 
