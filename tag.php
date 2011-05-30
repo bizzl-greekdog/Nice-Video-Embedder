c5e00691c80af65db9061fbd76290b358111d606
Benjamin Kleiner <bizzl@users.sourceforge.net>
2011-05-30 12:22:54 +0200

    Extended tag.php

--- tag.php
+++ tag.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!function_exists('tag')) {
 	class Tag {
 		private $name = '';
@@ -6,6 +7,7 @@ if (!function_exists('tag')) {
 		private $attributes = array();
 		private $children = array();
 		private $style = array();
+		private $classes = array();
 		
 		private function parseCSS($input) {
 			$input = strpos($input, ';') > -1 ? explode($input, ';') : array($input);
@@ -30,11 +32,17 @@ if (!function_exists('tag')) {
 			else
 				if ($value == 'style')
 					return clone $this->style;
+				elseif ($value == 'class')
+					return clone $this->classes;
 				else
 					return $this->attributes[$name];
 			if (isset($this->attributes['style'])) {
 				$this->parseCSS($this->attributes['style']);
 				unset($this->attributes['style']);
+			}
+			if (isset($this->attributes['class'])) {
+				$this->addClass($this->attributes['class']);
+				unset($this->attributes['class']);
 			}
 			return $this;
 		}
@@ -48,6 +56,37 @@ if (!function_exists('tag')) {
 				$this->parseCSS($name);
 			else
 				return $this->style[$name];
+			return $this;
+		}
+		
+		private function cleanClasses($args) {
+//			$args = func_get_args();
+			$classes = array();
+			foreach ($args as $arg)
+				if (is_array($arg))
+					$classes = array_merge($classes, $this->cleanClasses($arg));
+				elseif (strstr($arg, ' ') > -1)
+					$classes = array_merge($classes, $this->cleanClasses(explode(' ', $arg)));
+				else
+					$classes[] = $arg;
+			return $classes;
+		}
+		
+		public function addClass() {
+			$classes = func_get_args();
+			$this->classes = array_merge($this->classes, $this->cleanClasses($classes));
+			$this->classes = array_unique($this->classes);
+			return $this;
+		}
+		
+		public function removeClass($classes) {
+			if (!is_array($classes))
+				if (strstr($classes, ' ') > -1)
+					$classes = explode(' ', $classes);
+				else
+					$classes = array($classes);
+			foreach ($classes as $class)
+			$this->classes = array_diff($this->classes, $classes);
 			return $this;
 		}
 
@@ -83,6 +122,9 @@ if (!function_exists('tag')) {
 				$css = implode('; ', $css);
 				$result .= ' style="' . htmlentities2($css) . '"';
 			}
+			if (count($this->classes)) {
+				$result .= ' class="' . htmlentities2(implode(' ', $this->classes)) . '"';
+			}
 			if (count($this->children) || $this->forceClose) {
 				$result .= '>';
 				foreach ($this->children as $child)
@@ -96,6 +138,33 @@ if (!function_exists('tag')) {
 
 	function tag($tagName, $forceClose = false) {
 		return new Tag($tagName, $forceClose);
+	}
+	
+	function br() {
+		return new Tag('br', false);
+	}
+	
+	function hr() {
+		return new Tag('hr', false);
+	}
+	
+	function div() {
+		return new Tag('div', true);
+	}
+	
+	function span() {
+		return new Tag('span', true);
+	}
+	
+	function iframe() {
+		return new Tag('iframe', true);
+	}
+	
+	function script($src = null) {
+		$m = new Tag('script', true);
+		if ($src)
+			$m->attr('src', $src);
+		return $m->attr('type', 'text/javascript');
 	}
 }
 ?>
